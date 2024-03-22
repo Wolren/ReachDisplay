f76f38ea90e095bf527764bf3bb6bf20f3b8fbea
Wolren <86470291+Wolren@users.noreply.github.com>
2024-03-22 18:43:11 +0100

    1.17-1.18

---
+++ src/main/java/net/wolren/reach_display/data/SharedData.java
@@ -0,0 +1,32 @@
+package net.wolren.reach_display.data;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+
+public class SharedData {
+    private static SharedData instance;
+    private double distance;
+    private Entity entity;
+
+    private SharedData() {}
+
+    public static SharedData getInstance() {
+        if (instance == null) {
+            instance = new SharedData();
+        }
+        return instance;
+    }
+
+    public void setDistanceAndTarget(double distance, Entity entity) {
+        this.distance = distance;
+        this.entity = entity;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public Entity getEntity() {
+        return entity;
+    }
+}
